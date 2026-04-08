ac52e1957af3f51f8dfa199c7eab26654ca7cee7
Məftun Məmmədov <mftnmusic6386@gmail.com>
2026-04-08 23:02:48 +0400

    movie-card-updated

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,6 +1,6 @@
 import { Movie } from "@/types";
 import { Link } from "expo-router";
-import { DimensionValue, Image, StyleSheet } from "react-native";
+import { DimensionValue, Image, StyleSheet, View } from "react-native";
 
 const MovieCard = ({
   movie,
@@ -12,12 +12,11 @@ const MovieCard = ({
   width?: DimensionValue;
 }) => {
   return (
-    <Link
-      href={`/movies/${movie.slug}`}
-      style={[styles.movieCard, { marginRight, width }]}
-    >
-      <Image source={{ uri: movie.poster }} style={styles.movieCardPoster} />
-    </Link>
+    <View style={[styles.movieCard, { marginRight, width }]}>
+      <Link href={`/movies/${movie.slug}`}>
+        <Image source={{ uri: movie.poster }} style={styles.movieCardPoster} />
+      </Link>
+    </View>
   );
 };
 
@@ -27,8 +26,8 @@ export const styles = StyleSheet.create({
   movieCard: {
     aspectRatio: "2/3",
     borderRadius: 8,
-    overflow: "hidden",
     height: "auto",
+    overflow: "hidden",
   },
   movieCardPoster: {
     width: "100%",
